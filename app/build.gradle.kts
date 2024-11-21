c7755f339b35201e4a83e68cac7d8f96a82e0eb1
Nicolau Almeida <nicolasalmeidaalves@hotmail.com>
2024-11-21 17:13:51 -0300

    Primeiro commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,12 @@ android {
 
 dependencies {
 
+    annotationProcessor(libs.androidx.room.compiler)
+    implementation(libs.firebase.auth)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.firebase.database)
+    implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -59,6 +65,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
